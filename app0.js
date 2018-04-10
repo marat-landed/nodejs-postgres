5c57c193477949d0be00368f6eeb630c327fd3a4
marat-landed <mrtland7@gmail.com>
2018-04-10 10:08:17 +0300

    10-04-2018 10:08

--- app0.js
+++ app0.js
@@ -4,8 +4,17 @@ var app = express();
 const pg = require('pg');
 const { Client, Query } = require('pg');
 
+//const connectString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
 /*
-var connectString = {
+const connectString = {
+  user: 'postgres',
+  password: 'postgres',	
+  database: 'postgres'
+};
+*/
+console.log(process.env.POSTGRESQL_USER,process.env.POSTGRESQL_PASSWORD,process.env.POSTGRESQL_DATABASE);
+if (process.env.POSTGRESQL_USER) {
+  var connectString = {
     user: process.env.POSTGRESQL_USER,
     password: process.env.POSTGRESQL_PASSWORD,	
     database: process.env.POSTGRESQL_DATABASE
@@ -17,16 +26,32 @@ var connectString = {
   var postgresqlPassword = process.env["POSTGRESQL_PASSWORD"];
 
   var connectString = "postgresql://" + postgresqlUser + ":" + postgresqlPassword + "@" + postgresqlServiceHost + "/" + postgresqlDatabase;
-  var connectString = "postgresql://" + postgresqlUser + ":" + postgresqlPassword + "@" + postgresqlServiceHost + ":" + postgresqlServicePort;
+  var connectString = "postgresql://" + postgresqlUser + ":" + postgresqlPassword + "@" + postgresqlServiceHost + ":" + postgresqlServicePort;	
+  // const connectString = postgresql://user:password@host:port
+  // const connectString = postgres://user:password@hostname/database	
+	
+  /*	
+  //mongodb configuration
+  var mongoHost = process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost';
+  var mongoPort = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
+  var mongoUser = ''; //mongodb username
+  var mongoPass = ''; //mongodb password
+  var mongoDb   = ''; //mongodb database name
 
-*/
+  //mysql configuration
+  var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost';
+  var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT || 3306;
+  var mysqlUser = ''; //mysql username
+  var mysqlPass = ''; //mysql password
+  var mysqlDb   = ''; //mysql database name
+  */
 
   //postgresql configuration
   var postgresqlHost = process.env.OPENSHIFT_POSTGRESQL_DB_HOST || 'localhost'; 
   var postgresqlPort = process.env.OPENSHIFT_POSTGRESQL_DB_PORT || 5432;
-  var postgresqlUser = process.env["POSTGRESQL_USER"] || 'postgres'; //postgresql username
-  var postgresqlPass = process.env["POSTGRESQL_PASSWORD"] || 'postgres'; //postgresql password
-  var postgresqlDb   = process.env["POSTGRESQL_DATABASE"] || 'postgres'; //postgresql database name	
+  var postgresqlUser = process.env["POSTGRESQL_USER"]; //mysql username
+  var postgresqlPass = process.env["POSTGRESQL_PASSWORD"]; //mysql password
+  var postgresqlDb   = process.env["POSTGRESQL_DATABASE"]; //mysql database name	
   // Connection URL: postgresql://$OPENSHIFT_POSTGRESQL_DB_HOST:$OPENSHIFT_POSTGRESQL_DB_PORT/
   // echo $OPENSHIFT_POSTGRESQL_DB_PORT ----> 5432	
   // Connection URL: postgresql://$OPENSHIFT_POSTGRESQL_DB_HOST:$OPENSHIFT_POSTGRESQL_DB_PORT	
@@ -35,7 +60,17 @@ var connectString = {
   //var mongoString = 'mongodb://' + mongoUser + ':' + mongoPass + '@' + mongoHost + ':' + mongoPort + '/' + mongoDb;
   //var mysqlString = 'mysql://' + mysqlUser + ':' + mysqlPass + '@' + mysqlHost + ':' + mysqlPort + '/' + mysqlDb;
   var connectString = 'postgresql://' + postgresqlUser + ':' + postgresqlPass + '@' + postgresqlHost + ':' + postgresqlPort + '/' + postgresqlDb;	
-console.log("connectString:");
+	
+} else {
+  var connectString = {
+    user: 'postgres',
+    password: 'postgres',	
+    database: 'postgres'
+  };	
+}	
+
+
+
 console.log(connectString);
 
 /*
